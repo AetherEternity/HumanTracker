b7882e7ad3f7f6af6b5ab3a95e760e6fba12dcd1
AetherEternity <swing056@gmail.com>
2023-01-26 18:26:19 +0300

    Update install.sh due to python2-pip deprecation
    python-pip is not available on modern Ubuntu

--- install.sh
+++ install.sh
@@ -6,10 +6,13 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
 sudo add-apt-repository universe
 sudo apt-get install -y powershell
-
 apt-get -y install build-essential libssl-dev libffi-dev python3-dev
 apt-get -y install python-dev python-setuptools
 apt-get -y install python-pip
+apt install wget
+wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
+python get-pip.py
+
 pip2 install openpyxl
 pip2 install pillow
 
